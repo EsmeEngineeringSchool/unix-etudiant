56b4bfc56476c85c4ed9ccd204e1821d3d3e396c
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2021-08-31 14:10:55 +0200

    new restart

--- seance9/exemple_case.sh
+++
@@ -1,8 +0,0 @@
-opts=$1
-case $opts in 
--h)
-    echo "menu d'aide"
-    echo "exemple_case.sh -e ENTIER";;
--e) 
-    echo "l'entier $2 est entrée en argument";;
-esac
--- seance9/roman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ $1 =~ ^(V?(I{0,3})?|I[XV])$ ]]
-then
-    echo "$1 is roman"
-else
-    echo "$1 is not roman"
-fi
