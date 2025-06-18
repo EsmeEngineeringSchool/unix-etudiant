d4f00400aacb65d73ab191bdc6c787afb3f87f0c
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2025-06-18 11:35:57 +0200

    rebase continue

--- seance11/code.c
+++ seance11/code.c
@@ -12,10 +12,6 @@ int main(void)
     for (int i =0;i<n;i++)
     {
         // fonction d'affichage de chaines de caractères
-<<<<<<< HEAD
         printf("i = %d",i);
-=======
-        printf("i = %d",i)
->>>>>>> e5829d0 (maj 2022-2023 seance 11 -> seance 6)
     }
 }
